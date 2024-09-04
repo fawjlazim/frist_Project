85da61c72dc42dd007bc5c3fb392f3d91b8d5026
fawjlazim <mdfawjulazim.onlineid@gmail.com>
2024-09-04 12:25:18 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // Cutomize 
+        colors: {
+          'alaminColor': "#004b70",
+          'overlyColor': "rgba(0, 0, 0, 0.7);"
+        },
+        height: {
+            'aminH': '150px',
+          },
+          width: {
+            'aminW': '150px',
+          },
+          backgroundImage: {
+            'bannerImg': "url('./images/tanvir.jpg')",
+           
+          },
+          fontFamily: {
+            'vugijugi': ['Matemasie'],
+            
+          },
+          maxWidth: {
+            'bannerContainer': '1170px',
+          },
+          boxShadow: {
+            'asik': '-1px 10px 25px 0px rgba(0,0,0,0.75)',
+          }
+        // Cutomize 
+      }
+    }
+  }
